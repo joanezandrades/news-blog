121f9240be2ada564d18ac5ab467cb40aacef215
Joanez Andrades <me@joanezandrades.com>
2018-02-11 22:08:20 -0200

    Descrição single.php

--- single.php
+++ single.php
@@ -2,6 +2,7 @@
 /**
  * The single post template. Used when a single post is queried. For this and all other query templates, index.php is used if the query template is not present.
  * @version 1.0
+ * @author JoanezAndrades
 */
 get_header();
 ?>
